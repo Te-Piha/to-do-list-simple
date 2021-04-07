9eacd3ad924fda4e77fc5a348eb23fd56a31cdaf
Te-Piha <ttrwwn@hotmail.com>
2021-04-07 22:09:25 +1200

    work on inital functions

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function HomePage() {
+
+const [task, setNewTask] = useState('')
+
+
+
+const setTask = e => {
+  const newTaskboi = document.getElementById("task").value
+
+  setNewTask(newTaskboi)
+}
+
+
   return (
     <div>
-      
+      <h2>
+        To Do List
+      </h2>
+        <div> 
+            Todays Task 
+        </div>
+
+        <label>
+          {/* a place to type task */}
+          <input id="task" type="text"/>
+
+          {/* a place to submit */}
+          <button onClick={e => setTask()}> Add Task </button>
+    
+        </label>
+        <div>
+          <h2 value={task}> {task} </h2>
+        </div>
     </div>
   )
 }
